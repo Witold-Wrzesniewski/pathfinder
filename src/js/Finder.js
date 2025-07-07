0193de55bdf56f16e99c8dad4694798d3fc074b7
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-07-07 17:25:44 +0200

    Add Finder class

---
+++ src/js/Finder.js
@@ -0,0 +1,8 @@
+class Finder {
+  constructor(element){
+    const thisFinder = this;
+
+  }
+
+}
+export default Finder;
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import Finder from './Finder.js';
+
+const app = {
+  init: function(element){
+    const page = new Finder(element);
+
+  }
+}
+app.init();
